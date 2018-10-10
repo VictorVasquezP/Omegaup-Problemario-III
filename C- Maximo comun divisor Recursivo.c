8d705b6caa021f1a7fc0c3f13405164d1ec7bac6
VictorVasquezP <42286571+VictorVasquezP@users.noreply.github.com>
2018-10-09 19:18:55 -0500

    Add files via upload

---
+++ C- Maximo comun divisor Recursivo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long mcd(int a, int b);
+int main(int argc, char *argv[]) {
+	int b,a,t;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	t=mcd(a,b);
+	printf("%d",t);
+	return 0;
+}
+long mcd(int a, int b){
+	if(b==0){
+		return a;
+	}else{
+		return mcd(b,a%b);
+	}
+}
+
